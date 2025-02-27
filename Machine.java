d19c8689883ed9914fe2398b44a5501880c59efb
markmccreary00 <m274230@usna.edu>
2025-02-27 10:33:01 -0500

    add stubs

--- Machine.java
+++ Machine.java
@@ -1,5 +1,17 @@
 public class Machine {
-    public double play(){
+    public double play() {
         return 1.25;
-    }    
+    }
+
+    public String getName() {
+        return "stub name";
+    }
+
+    public boolean inService() {
+        return true;
+    }
+
+    public void report() {
+        System.out.println("stub report");
+    }
 }
