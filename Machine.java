54a876d6f37d9f5cc9efa99a1d5fef37397d2e51
markmccreary00 <m274230@usna.edu>
2025-02-27 11:06:49 -0500

    add stubs

--- Machine.java
+++ Machine.java
@@ -1,6 +1,35 @@
+import java.util.*;
+
 public class Machine {
-    public double play() {
-        return 1.25;
+    private String name;
+    private double house = 0;
+    private double pot, payout;
+    boolean toPot = false;
+
+    public Machine(String n, double h, double p) {
+        name = n;
+        house = h;
+        payout = p;
+    }
+
+    public double play(Boolean result) {
+        if()
+        if(toPot){
+            pot += 0.25;
+        }
+        else{
+            house += 0.25;
+        }
+        toPot = !toPot;
+
+        if(result){
+            house -= payout;
+            return payout;
+        }
+        else{
+            return 0;
+        }
+        
     }
 
     public String getName() {
