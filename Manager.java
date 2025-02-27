2136dad72944281fe7e132377d849a08183bd984
m273336 <m273336@usna.edu>
2025-02-27 10:32:03 -0500

    add Manager stub

---
+++ Manager.java
@@ -0,0 +1,17 @@
+public class Manager {
+
+  public Manager(){
+    // This is a constructor for manager
+  }
+
+  // Draw money and machines
+  public void draw(){
+    System.out.println("TODO: call draw on machines");
+  }
+
+  // execute left/right/play/*report
+  public void exec(String comm){
+    System.out.println("TODO: executing command: " + comm); 
+  }
+  
+}
