f5d096e58df9a89b528b4665b8ca8b001a61eed8
m273336 <m273336@usna.edu>
2025-02-27 11:24:18 -0500

    added Draw class

--- Draw.java
+++ Draw.java
@@ -2,6 +2,43 @@ public class Draw{
 
   public static void show(Machine[] m, int pos){
     // Print out the boxes
-    System.out.println("Printing Machines");
+    for(int i = 0; i < 5; i ++){
+      for(int j = 0; j < m.length; j ++){
+	char c = (j == pos ? '#' : '-');
+	if(i == 0 || i == 4){
+	  for(int k = 0; k < 10; k ++){
+	    System.out.print(c);
+	  }
+	  System.out.print("  ");
+	}
+	if(i == 1 || i == 3){
+	  System.out.print(c + "        " + c + "  ");
+	}
+	if(i == 2){
+	  String name = m[j].getName();
+	  System.out.print(c);
+	  int n = (8 - name.length()) / 2;
+	  for(int k = 0; k < n; k ++){
+	    System.out.print(' ');
+	  }
+	  System.out.print(name);
+	  for(int k = 0; k < n; k ++){
+	    System.out.print(' ');
+	  }
+	  System.out.print(c + "  ");
+	}
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args){
+    Machine[] m = new Machine[5];
+    m[0] = new Machine();
+    m[1] = new Machine();
+    m[2] = new Machine();
+    m[3] = new Machine();
+    m[4] = new Machine();
+    show(m, 4);
   }
 }
