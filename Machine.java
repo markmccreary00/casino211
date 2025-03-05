e1fd6726f2414f14dc07b6daa03331722f322369
markmccreary00 <m274230@usna.edu>
2025-03-04 19:46:56 -0500

    added report functionality

--- Machine.java
+++ Machine.java
@@ -1,30 +1,36 @@
-import java.util.*;
-
 public class Machine {
     private String name;
 
     // house winnings default value is 0 upon Machine initialization
     private double house = 0;
-    private double pot, payout;
+    // initial pot value tracked to compute net value for report
+    private double initialPot, pot, payout;
 
     // controls behavior of quarter going to either pot or house
     boolean toPot = false;
 
     // basic constructor for machine
-    public Machine(String n, double h, double p) {
-        name = n;
-        house = h;
-        payout = p;
+    public Machine(String name, double pot, double payout) {
+        this.name = name;
+        this.pot = this.initialPot = pot;
+        this.payout = payout;
     }
 
     /*
+     * handler for changes in pot & house values.
      * intended execution flow:
-     * 1) determines where quarter goes to (pot or house)
-     * 2) using result argument, compute changes in pot
-     * 3) return double for payout; return value of 0 correlates to loss, 
+     * 1) check if machine is in service
+     * 2) determines where quarter goes to (pot or house)
+     * 3) using result argument, compute changes in pot
+     * 4) return double for payout; return value of 0 correlates to loss,
      * return value > 0 correlates to amount winnings
+     * return value == -1 correlates to out of service machine
      */
     public double play(boolean result) {
+        if(!inService()){
+            return -1;
+        }
+        
         if (toPot) {
             pot += 0.25;
         } else {
@@ -41,15 +47,22 @@ public class Machine {
 
     }
 
+    // simple name getter
     public String getName() {
-        return this.name;
+        return name;
     }
 
+    // method for checking machine operability state
     public boolean inService() {
-        return true;
+        if (pot < payout) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
-    public void report() {
-        System.out.println("stub report");
+    public String report() {
+        double net = house + pot - initialPot;
+        return String.format("casino: $%.2f, pot: $%.2f, net: $%.2f", house, pot, net);
     }
 }
